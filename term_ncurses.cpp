9b664796345743c3e6d1a17823652ddc6d19e15f
Koichi Murase <myoga.murase@gmail.com>
2013-05-20 02:41:14 +0900

    contra1: update term_ncurses.cpp

--- term_ncurses.cpp
+++ term_ncurses.cpp
@@ -1,7 +1,47 @@
+#include <cstdlib>
+#include <unistd.h>
+#include <mwg/except.h>
 #include "term_target.h"
 
+ITarget* CreateNcursesInstance(int fd,const char* TERM=nullptr);
+
 class NcursesOutput:public ITarget{
+  int fd;
 public:
-  virtual void flush()=0;
+  NcursesOutput(int fd,const char* TERM){
+    this->fd=fd;
+    mwg_assert(TERM!=nullptr&&::isatty(fd));
+  }
+private:
+  bool process_SGR(SequenceArgument const& arg){
+    if(args.size()==0){
+      this->putc('\33');
+      this->putc('[');
+      this->putc('m');
+    }
+    return true;
+  }
+public:
+  virtual bool process_function(ControlCodes func,SequenceArgument const& arg){
+    switch(func){
+    TTC_SGR:
+      return process_SGR(arg);
+    }
+    break;
+  }
+  virtual void putc(mwg::byte data){
+    //■TODO キャッシュ
+    ::write(fd,&data,1);
+  }
+  virtual void flush(){}
 };
 
+ITarget* CreateNcursesInstance(int fd,const char* TERM){
+  if(TERM==nullptr)
+    TERM=std::getenv("TERM");
+
+  if(TERM!=nullptr&&::isatty(fd))
+    return new NcursesOutput(fd,TERM);
+  else
+    return nullptr;
+}
