2a5c4bf73301d86680f648761450ed9846a26c54
Koichi Murase <myoga.murase@gmail.com>
2016-09-20 05:20:23 +0900

    test (terminal_target): add support to output SGR

--- test.cpp
+++ test.cpp
@@ -16,16 +16,24 @@ namespace tty1 {
   enum cell_attribute {
     fg_color_mask = 0x00FF,
     bg_color_mask = 0xFF00,
+    fg_color_shift = 0,
+    bg_color_shift = 8,
 
-    is_fg_color_set  = 0x00010000,
-    is_bg_color_set  = 0x00020000,
-    is_bold_set      = 0x00040000,
-    is_underline_set = 0x00080000,
-    is_stroke_set    = 0x00100000,
-    is_invisible_set = 0x00200000,
-    is_italic_set    = 0x00400000,
+    is_fg_color_set         = 0x00010000,
+    is_bg_color_set         = 0x00020000,
+    is_bold_set             = 0x00040000, // ANSI \e[1m
+    is_faint_set            = 0x00080000, // ANSI \e[2m
+    is_italic_set           = 0x00100000, // ANSI \e[3m
+    is_underline_set        = 0x00200000, // ANSI \e[4m
+    is_blink_set            = 0x00400000, // ANSI \e[5m
+    is_rapid_blink_set      = 0x00800000, // ANSI \e[6m
+    is_reverse_set          = 0x01000000, // ANSI \e[7m
+    is_invisible_set        = 0x02000000, // ANSI \e[8m
+    is_strike_set           = 0x04000000, // ANSI \e[9m
+    is_fraktur_set          = 0x08000000, // ANSI \e[20m
+    is_double_underline_set = 0x10000000, // ANSI \e[21m
 
-    has_extended_attribute = 0x8000,
+    has_extended_attribute = 0x8000, // not supported yet
   };
 
   struct window_cell {
@@ -106,6 +114,148 @@ struct terminal_target {
 
   terminal_target(std::FILE* file): file(file) {}
 
+  void put_str(const char* s) {
+    while (*s) std::fputc(*s++, file);
+  }
+  void put_unsigned(unsigned value) {
+    if (value >= 10)
+      put_unsigned(value/10);
+    std::fputc('0' + value % 10, file);
+  }
+
+  void sa_open_sgr() {
+    if (this->sa_isSgrOpen) {
+      std::fputc(';', file);
+    } else {
+      this->sa_isSgrOpen = true;
+      std::fputc('\033', file);
+      std::fputc('[', file);
+    }
+  }
+
+
+  std::uint32_t attr {0};
+  bool sa_isSgrOpen;
+
+  struct sgrcap_color_type {
+    int base;
+    int bright_base;
+    bool iso8613_indexed_color;
+    char iso8613_separater;
+  };
+
+  struct sgrcap_type {
+    sgrcap_color_type sgrfg {30,  90, true, ';'};
+    sgrcap_color_type sgrbg {40, 100, true, ';'};
+  } sgrcap;
+
+  void update_sgrflag1(
+    std::uint32_t newAttr,
+    std::uint32_t bit, unsigned sgr, unsigned sgrReset
+  ) {
+    if (((newAttr ^ this->attr) & bit) == 0) return;
+
+    sa_open_sgr();
+    if (newAttr&is_reverse_set) {
+      put_unsigned(sgr);
+    } else {
+      put_unsigned(sgrReset);
+    }
+  }
+
+  void update_sgrflag2(
+    std::uint32_t newAttr,
+    std::uint32_t bit1, unsigned sgr1,
+    std::uint32_t bit2, unsigned sgr2,
+    unsigned sgrReset
+  ) {
+    if (((newAttr ^ this->attr) & (bit1 | bit2)) == 0) return;
+
+    std::uint32_t const added = newAttr & ~this->attr;
+    std::uint32_t const removed = ~newAttr & this->attr;
+
+    sa_open_sgr();
+    if (removed & (bit1 | bit2)) {
+      put_unsigned(sgrReset);
+      if (newAttr&bit2) {
+        std::fputc(';', file);
+        put_unsigned(sgr2);
+      }
+      if (newAttr&bit1) {
+        std::fputc(';', file);
+        put_unsigned(sgr1);
+      }
+    } else {
+      bool isOutput = false;
+      if (added & bit2) {
+        put_unsigned(sgr2);
+        isOutput = true;
+      }
+      if (added & bit1) {
+        if (isOutput) std::fputc(';', file);
+        put_unsigned(sgr1);
+      }
+    }
+  }
+
+  void update_color(std::uint32_t newAttr, sgrcap_color_type const& cap, int isset, int mask, int shift) {
+    if (((this->attr ^ newAttr) & (isset | mask)) == 0) return;
+
+    std::uint32_t const added = newAttr & ~this->attr;
+    std::uint32_t const removed = ~newAttr & this->attr;
+    if (removed & isset) {
+      sa_open_sgr();
+      put_unsigned(cap.base + 9);
+    } else if (added & isset || (this->attr ^ newAttr) & mask ) {
+      sa_open_sgr();
+      int const fg = unsigned(newAttr & mask) >> shift;
+      if (fg < 8) {
+        // e.g \e[31m
+        put_unsigned(cap.base + fg);
+      } else if (fg < 16 && cap.bright_base) {
+        // e.g. \e[91m
+        put_unsigned(cap.bright_base + (fg & 7));
+      } else if (cap.iso8613_indexed_color) {
+        // e.g. \e[38;5;17m
+        put_unsigned(cap.base + 8);
+        std::fputc(cap.iso8613_separater, file);
+        put_unsigned(5);
+        std::fputc(cap.iso8613_separater, file);
+        put_unsigned(fg);
+      } else {
+        // todo: fallback
+        // todo: \e[1;32m で明るい色を表示する端末
+        put_unsigned(cap.base + 9);
+      }
+    }
+  }
+  void set_attribute(std::uint32_t newAttr) {
+    if (this->attr == newAttr) return;
+
+    this->sa_isSgrOpen = false;
+
+    if (newAttr == 0) {
+      sa_open_sgr();
+    } else {
+      update_sgrflag2(newAttr, is_bold_set     , 1, is_faint_set           ,  2, 22);
+      update_sgrflag2(newAttr, is_italic_set   , 3, is_fraktur_set         , 20, 23);
+      update_sgrflag2(newAttr, is_underline_set, 4, is_double_underline_set, 21, 24);
+      update_sgrflag2(newAttr, is_blink_set    , 5, is_rapid_blink_set     ,  6, 25);
+
+      update_sgrflag1(newAttr, is_reverse_set  , 7, 27);
+      update_sgrflag1(newAttr, is_invisible_set, 8, 28);
+      update_sgrflag1(newAttr, is_strike_set   , 9, 29);
+
+      update_color(newAttr, sgrcap.sgrfg, is_fg_color_set, fg_color_mask, fg_color_shift);
+      update_color(newAttr, sgrcap.sgrbg, is_bg_color_set, bg_color_mask, bg_color_shift);
+    }
+
+    if (this->sa_isSgrOpen)
+      std::fputc('m', file);
+
+    this->attr = newAttr;
+  }
+
   // test implementation
   // ToDo: output encoding
   void output_content(window& w) {
@@ -120,17 +270,18 @@ struct terminal_target {
         }
 
         if (wskip > 0) {
-          if (wskip <= 4) {
-            while (wskip--) std::putc(' ', file);
+          if (this->attr == 0 && wskip <= 4) {
+            while (wskip--) std::fputc(' ', file);
           } else
             std::fprintf(file, "\033[%dC", wskip);
           wskip = 0;
         }
 
-        std::putc(line[x].character, file);
+        set_attribute(line[x].attribute);
+        std::fputc(line[x].character, file);
       }
 
-      std::putc('\n', file);
+      std::fputc('\n', file);
     }
   }
 
@@ -141,14 +292,19 @@ int main() {
   tty1::window w;
   w.resize(10, 10);
 
+  w.cur.attribute = 196;
   for (int i = 0; i < 26; i++)
     put_char(w, 'a' + i);
+  w.cur.attribute = 202;
   for (int i = 0; i < 26; i++)
     put_char(w, 'a' + i);
+  w.cur.attribute = 220;
   for (int i = 0; i < 26; i++)
     put_char(w, 'a' + i);
+  w.cur.attribute = 154;
   for (int i = 0; i < 26; i++)
     put_char(w, 'a' + i);
+  w.cur.attribute = 63;
   for (int i = 0; i < 26; i++)
     put_char(w, 'a' + i);
 
