762af27b9a2353469bb794c125f3c062cb58f291
Koichi Murase <myoga.murase@gmail.com>
2021-02-26 23:56:11 +0800

    main: support "contra test width height"

--- src/contra.cpp
+++ src/contra.cpp
@@ -43,6 +43,34 @@ int main(int argc, char** argv) {
     if (!contra::tx11::run(actx)) return 1;
   } else if (std::strcmp(argv[1], "tty") == 0) {
     if (!contra::ttty::run(actx)) return 1;
+  } else if (std::strcmp(argv[1], "test") == 0) {
+    using curpos_t = contra::ansi::curpos_t;
+
+    const char* sw = 2 < argc ? argv[2] : std::getenv("COLUMNS");
+    const char* sh = 3 < argc ? argv[3] : std::getenv("LINES");
+    int const w = std::clamp(sw ? std::atoi(sw) : 80, 1, 9999);
+    int const h = std::clamp(sh ? std::atoi(sh) : 24, 1, 9999);
+
+    contra::ansi::term_t term(w, h);
+
+    // 標準入力を全て term に食わせる
+    for (int c; (c = std::getchar()) != EOF; ) {
+      char b = (char) c;
+      term.write(&b, sizeof b);
+    }
+
+    // 中身を標準出力に書き出し
+    auto const& board = term.board();
+    for (curpos_t y = 0; y < board.height(); y++) {
+      auto const& line = board.line(y);
+      for (curpos_t x = 0; x < board.width(); x++) {
+        char32_t c = line.char_at(x).value;
+        if (!(c & contra::charflag_wide_extension))
+          contra::encoding::put_u8(c == U'\0' ? U' ' : c, stdout);
+      }
+      std::fputc('\n', stdout);
+    }
+
   } else if (std::strcmp(argv[1], "--help") == 0) {
     std::cout
 #ifdef use_twin
